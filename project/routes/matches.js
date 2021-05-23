8bae237731079fbfdc0dab812885db4f6e34323e
isgales <alkalayg@post.bgu.ac.il>
2021-05-23 16:27:22 +0300

    same as last comit

--- project/routes/matches.js
+++ project/routes/matches.js
@@ -3,13 +3,26 @@ var router = express.Router();
 const DButils = require("./utils/DButils");
 // const matches_utils = require("./utils/matches_utils");
 
+router.get('/:match_id',async(req, res, next) => {
+
+    try {
+        const data = await DButils.execQuery(
+            `SELECT * FROM dbo.Matches WHERE Match_ID = '${req.params.match_id}'`
+        )
+        res.send(data);
+
+    } catch(error) {
+        next(error);
+    }
+});
+
 router.use(async(req, res, next) => {
     try {
+        let user_name = req.body.user_name || req.query.user_name
         let role = (await DButils.execQuery(
-            `SELECT User_Role FROM dbo.Users WHERE Username = '${req.body.username}'`
+            `SELECT User_Role FROM dbo.Users WHERE Username = '${user_name}'`
         ))[0].User_Role;
         if (role != 'Representative') {
-            console.log(role)
             throw 'Invalid access!'
         }
         next();
@@ -19,13 +32,26 @@ router.use(async(req, res, next) => {
     }
 });
 
-router.post(async(req, res, next) => {
+router.put('/',async(req, res, next) => {
     try {
-        console.log('in post')
+        const query = await DButils.execQuery(
+            `UPDATE dbo.Matches SET 
+            Score = '${req.body.score}',
+            EventBook = '${req.body.event_book}' 
+            WHERE Match_ID = '${req.body.match_id}'`
+        )
+        res.status(200).send("match updated");
+
+    } catch(error) {
+        next(error);
+    }
+});
+
+router.post('/',async(req, res, next) => {
+    try {
         const match_id = (await DButils.execQuery(
             "SELECT COUNT(*) FROM dbo.Matches"
         ))[0][""];
-        console.log(match_id)
         const query = await DButils.execQuery(
             `INSERT INTO dbo.Matches (Match_ID, Home_Team_ID, Away_Team_ID, Match_Date, Stadium)
          VALUES ('${match_id}','${req.body.home_team_id}',
@@ -33,21 +59,24 @@ router.post(async(req, res, next) => {
          '${req.body.date}',
          '${req.body.stadium}')`
         )
-        console.log('query executed')
         res.status(201).send("match created");
 
     } catch(error) {
         next(error);
     }
 });
-/* 
-    for postman json post:
-        {
-        "username" : "Ariel",
-        "home_team_id" : "",
-        "away_team_id" : "",
-        "date" : "12/04/2021",
-        "stadium" : "sami"
-        }
-*/
+
+router.delete('/:match_id',async(req, res, next) => {
+
+    try {
+        const data = await DButils.execQuery(
+            `DELETE FROM dbo.Matches WHERE Match_ID = '${req.params.match_id}'`
+        )
+        res.status(200).send("match deleted!")
+
+    } catch(error) {
+        next(error);
+    }
+});
+
 module.exports = router;
