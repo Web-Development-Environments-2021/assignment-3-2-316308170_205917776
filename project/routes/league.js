f131390bf7e0daa448205f06c7162886111cbfdc
shiloari <shiloari@post.bgu.ac.il>
2021-05-27 11:58:30 +0300

    update: league

--- project/routes/league.js
+++ project/routes/league.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const league_utils = require("./utils/league_utils");
 
+
 router.get("/getDetails", async(req, res, next) => {
     try {
         const league_details = await league_utils.getLeagueDetails();
@@ -9,6 +10,17 @@ router.get("/getDetails", async(req, res, next) => {
     } catch (error) {
         next(error);
     }
+});
+
+router.get('/get_upcoming_game', async(req, res, next) => {
+    try {
+        const current_stage_id = await league_utils.getLeagueDetails().current_stage_id;
+        const upcoming_game = await league_utils.getUpcomingGame(current_stage_id);
+        res.send(upcoming_game);
+    } catch (error) {
+        next(error);
+    }
+
 });
 
 module.exports = router;
--- project/routes/utils/league_utils.js
+++ project/routes/utils/league_utils.js
@@ -1,6 +1,21 @@
 const axios = require("axios");
 const LEAGUE_ID = 271;
 const api_domain = "https://soccer.sportmonks.com/api/v2.0";
+const DButils = require('./DButils')
+
+async function getUpcomingGame(stage_id) {
+    const stage_order = (await axios.get(
+        `${api_domain}/stages/${stage_id}`, {
+            params: {
+                api_token: process.env.api_token,
+            },
+        }
+    )).data.sort_order;
+    upcoming_game = DButils.execQuery(
+        `SELECT TOP 1 FROM dbo.Matches WHERE Stage = '${stage_order}' 
+        ORDER BY Match_Date`
+    );
+}
 
 async function getLeagueDetails() {
     const league = await axios.get(
@@ -23,8 +38,10 @@ async function getLeagueDetails() {
         current_season_name: league.data.data.season.data.name,
         current_season_id: league.data.data.season.data.id,
         current_stage_name: stage.data.data.name,
+        current_stage_id: stage.data.data.id
 
         // next game details should come from DB
     };
 }
 exports.getLeagueDetails = getLeagueDetails;
+exports.getUpcomingGame = getUpcomingGame;
