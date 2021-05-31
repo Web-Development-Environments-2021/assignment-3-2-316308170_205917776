cea7e275ac19732965421fb8efc53d46b85dfbdb
isgales <alkalayg@post.bgu.ac.il>
2021-05-31 19:05:31 +0300

    start test + pack

---
+++ project/app.test.js
@@ -0,0 +1,10 @@
+
+const request = require('supertest');
+const app = require('./main');
+jest.useFakeTimers()
+
+describe('TODO API', () => {
+    it('GET /todos --> array todos', (done) =>{
+        done();
+    })
+})
